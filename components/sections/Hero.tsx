70e4310f1b1a573cb19d7ca6ca0a6d118194115e
Endekalu Zemenu <endekaluzemenu2134@gmail.com>
2025-08-05 19:15:13 +0300

    feat(hero + animation): implement GSAP animation for character reveal effect

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,8 +1,30 @@
+"use client"
+
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { SplitText } from "gsap/all"
+
 const Hero = () => {
+	useGSAP(() => {
+		const splitTextName = SplitText.create(".name", { type: "chars", tag: "span", charsClass: "char" });
+
+		gsap.from(splitTextName.chars, {
+			opacity: 0,
+			stagger: {
+				amount: 0.5,
+				from: "random"
+			},
+			filter: "blur(16px)",
+			ease: "power1.inOut",
+			duration: 1,
+			scale: 1.1
+		});
+	})
+
 	return (
 		<section>
 			<div className="flex flex-col items-center justify-between leading-0 min-h-dvh py-28">
-				<h1 className="text-[clamp(3rem,15vw,250px)] font-medium leading-none text-primary font-korium">
+				<h1 className="text-[clamp(3rem,15vw,250px)] font-medium leading-none text-primary font-korium name">
 					ENDEKALU ZEMENU
 				</h1>
 				<div className="self-center text-center font-medium tracking-tighter flex flex-col text-primary gap-2">
@@ -25,5 +47,3 @@ const Hero = () => {
 }
 
 export default Hero
-
-//Code, design, and flow — all in one mind
