f04edf5f96080dd415339649e3c5b1361db06423
Endekalu Zemenu <endekaluzemenu2134@gmail.com>
2025-12-03 05:29:29 +0300

    chore: change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
     <html lang="en">
       <Lenis>
         <body
-          className={`${BricolageGrotesqueSans.variable} ${koriumSans.variable} ${telmaSans.variable} font-sans antialiased`}
+          className={`${BricolageGrotesqueSans.className} ${koriumSans.variable} ${telmaSans.variable} font-sans antialiased`}
         >
           {children}
         </body>
